bba156cfc805fabbf327ecac17c94aef232c8bd7
kashishtam <kashishtam@gmail.com>
2023-04-11 21:21:21 -0600

    added clean emojis and converting to numerical inputs

--- PreProcess.py
+++ PreProcess.py
@@ -3,16 +3,19 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('omw-1.4')
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+import re
+# uncoment if not installed
+
+#nltk.download('omw-1.4')
+#nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('wordnet')
 
 class PreProcess:
     def __init__(self):
         self.stop_words = set(stopwords.words('english'))
         self.lemmatizer = WordNetLemmatizer()
-
+    
     def preprocess_text(self, text):
         # Tokenize the text into words
         words = word_tokenize(text)
@@ -30,3 +33,27 @@ class PreProcess:
         preprocessed_text = ' '.join(words)
 
         return preprocessed_text
+    
+    def clean_text(self,text):
+        #Define a regular expression to match emojis and the shruggie
+        emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\u00a9-\u1f1ff"        # miscellaneous symbols and flags
+                                "]+", flags=re.UNICODE)
+
+        # Remove emojis and the shruggie from the text
+        text = emoji_pattern.sub(r'', text)
+        text = re.sub(r'(?:@[\w_]+)', "", text)
+        text = re.sub(r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', "", text)
+        text = text.replace('_', ' ')
+        text = text.replace('#', ' ')
+        text = text.replace(u"\u30c4",' ')
+        text = text.replace('(',' ')
+        text = text.replace(')',' ')
+        text = text.replace(u"\u005c",' ')
+        text = text.replace(u"\u002f",' ')
+
+        return text
--- main.py
+++ main.py
@@ -3,15 +3,30 @@
 from DataLoader import CSVReader
 from PreProcess import PreProcess
 
+from transformers import BertTokenizer
+
 reader = CSVReader("train/train.EN.csv")
 reader.read_file()
-text = reader.get_sentence_column(1)
+text = reader.get_sentence_column(4)
 print(text)
 
 process = PreProcess()
 processed_text = process.preprocess_text(text)
+processed_text = process.clean_text(processed_text)
 print(processed_text)
 
+# Load the BERT tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Tokenize the text
+tokens = tokenizer.tokenize(processed_text)
+
+# Convert the tokenized text into numerical inputs
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+# Print the tokenized text and numerical inputs
+print(f"Tokenized text: {tokens}")
+print(f"Numerical inputs: {input_ids}")
 
 
 
